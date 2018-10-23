360e0c443007ed90ed92c9453bb0da31afff6611
Grigler <graham.rigler@gmail.com>
2018-10-23 10:05:12 +0100

    Initial commit

---
+++ src/example/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+  printf("This is an example\n");
+  
+  return 0;
+}
---
+++ src/tests/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+  printf("This is an actual test\n");
+
+  return 0;
+}
