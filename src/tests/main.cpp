6dab31cb0ac5ce32e28c849383db12254299eb39
Grigler <graham.rigler@gmail.com>
2018-10-23 10:27:51 +0100

    working gtest

--- src/tests/main.cpp
+++ src/tests/main.cpp
@@ -1,8 +1,18 @@
-#include <iostream>
+#include "gtest/gtest.h"
 
-int main()
+
+int foo(int _a, int _b)
 {
-  printf("This is an actual test\n");
+  return _a*_b;
+}
 
-  return 0;
+TEST(TestingTheTests, DoesNothing)
+{
+  EXPECT_EQ(foo(10,10),100);
+  ASSERT_EQ(foo(999,0),0);
+}
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
